423ddb7a5b67134958f0cf1104e873aa43f246e6
Bryce Stevenson <bstevenson@smu.edu>
2016-10-27 20:21:49 -0500

    Issue #13. Created a email-picker component based off the location picker component

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -16,18 +16,20 @@ var app_component_1 = require('./app.component');
 var home_page_component_1 = require('./components/home-page/home-page.component');
 var create_event_poll_form_component_1 = require('./components/create-event-poll-form/create-event-poll-form.component');
 var location_picker_component_1 = require('./components/location-picker/location-picker.component');
+var email_picker_component_1 = require('./components/email-picker/email-picker.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, forms_1.FormsModule,
+            imports: [platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
                 router_1.RouterModule.forRoot([
                     { path: '', component: home_page_component_1.HomePageComponent },
                     { path: 'createpoll', component: create_event_poll_form_component_1.CreateEventPollFormComponent }
                 ])
             ],
-            declarations: [app_component_1.AppComponent, home_page_component_1.HomePageComponent, create_event_poll_form_component_1.CreateEventPollFormComponent, location_picker_component_1.LocationPickerComponent],
+            declarations: [app_component_1.AppComponent, home_page_component_1.HomePageComponent, create_event_poll_form_component_1.CreateEventPollFormComponent, location_picker_component_1.LocationPickerComponent, email_picker_component_1.EmailPickerComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
