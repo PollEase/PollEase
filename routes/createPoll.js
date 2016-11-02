4db9fc57d40e437569803d08517d24e293bcd3ba
Luke Wood <lswood@smu.edu>
2016-11-02 14:28:40 -0500

    uids

--- routes/createPoll.js
+++ routes/createPoll.js
@@ -1,16 +1,18 @@
 var sendemail = require("../helpers/sendmail.js");
-
+const crypto = require("crypto");
 function post(req,res){
     var email = req.body.creatorEmail;
     var name = req.body.creatorName;
-    var eventTitle = req.body.eventTitle;
+    var event_title = req.body.eventTitle;
     var deadline = req.body.pollDeadline;
     var locations = req.body.locations;
     var date = req.body.dateTime;
     var transportation = req.body.transportation;
     var funding = req.body.funding;
     var description = req.body.description;
-    var emails = req.body.emails;
+    var recipient_emails = req.body.emails;
+
+    var uid = crypto.randomBytes(16).toString("hex");
 
     res.send(`{
       "shareLink": "htp://dbgui1.com/event/poll/{pollId}",
