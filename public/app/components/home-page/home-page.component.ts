bffb2974489a310ea62ee7b3ff232b3781fdfac0
Bryce Stevenson <bstevenson@smu.edu>
2016-11-05 21:52:11 -0600

    Got bored made us a logo

--- public/app/components/home-page/home-page.component.ts
+++ public/app/components/home-page/home-page.component.ts
@@ -5,7 +5,6 @@ import { ActivatedRoute, Router, Params } from '@angular/router';
 	selector: 'home-page',
 	templateUrl: './app/components/home-page/home-page.component.html',
 	styleUrls: ['./app/components/home-page/home-page.component.css']
-	// encapsulation: ViewEncapsulation.None // Was causing an issue bringing the main page styles and img to the form page
 })
 
 export class HomePageComponent {
